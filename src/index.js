77fff2f1af2b3012741ed76b1b09b6477fa51786
ferucci <development.first1@gmail.com>
2022-05-14 23:33:57 +0300

    added a desktop version of the slider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 import modalPhone from './modules/modal';
 import advSlider from './modules/slider';
+import ourServices from './modules/sliderServices';
 
 modalPhone();
 advSlider();
+ourServices();
---
+++ src/modules/sliderServices.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const ourServicesSlider = () => {
+  const innerBlock = document.getElementById('services');
+  const wrapSlider = innerBlock.querySelector('.row');
+  const sliderItems = wrapSlider.querySelectorAll('.services__item');
+  const sliderArrows = wrapSlider.querySelectorAll('.services-arrows');
+
+  const toggleClass = () => {
+    sliderItems.forEach((item, index) => {
+      sliderItems[index].classList.toggle('services__item--active');
+    });
+  }
+
+  sliderArrows.forEach((btn, id) => {
+    btn.addEventListener('click', (e) => {
+      if (document.documentElement.clientWidth > 576) {
+        if (e.target.closest('.services__arrow--right')) {
+          toggleClass();
+        } else if (e.target.closest('.services__arrow--left')) {
+          toggleClass();
+        }
+      } else {
+
+      }
+
+    });
+  });
+}
+
+export default ourServicesSlider;
