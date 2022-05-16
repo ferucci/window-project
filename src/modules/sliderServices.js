034a459d2dd4cdbf3c2d4070829f6916705a35e7
ferucci <development.first1@gmail.com>
2022-05-16 14:18:57 +0300

    added slides block

--- src/modules/sliderServices.js
+++ src/modules/sliderServices.js
@@ -6,9 +6,36 @@ const ourServicesSlider = () => {
   const sliderItems = wrapSlider.querySelectorAll('.services__item');
   const sliderArrows = wrapSlider.querySelectorAll('.services-arrows');
 
+  let currentSlide = 0;
+
+  if (document.documentElement.clientWidth < 576) {
+    sliderItems[0].classList.add('services__item--active');
+    sliderItems[1].classList.remove('services__item--active');
+  }
+
+  const prevSlide = (elems, id, strClass) => {
+    sliderItems[currentSlide].classList.remove(strClass);
+    currentSlide--;
+    if (currentSlide < sliderItems.length) {
+      currentSlide = 0;
+    }
+    sliderItems[currentSlide].classList.add(strClass);
+  }
+
+  const nextSlide = (elems, id, strClass) => {
+    sliderItems[currentSlide].classList.remove(strClass);
+    currentSlide++;
+
+    if (currentSlide >= sliderItems.length) {
+      currentSlide = 0;
+    }
+    sliderItems[currentSlide].classList.add(strClass);
+  }
+
   const toggleClass = () => {
     sliderItems.forEach((item, index) => {
       sliderItems[index].classList.toggle('services__item--active');
+      console.log(sliderItems[index]);
     });
   }
 
@@ -21,7 +48,11 @@ const ourServicesSlider = () => {
           toggleClass();
         }
       } else {
-
+        if (e.target.closest('.services__arrow--right')) {
+          nextSlide(sliderItems, currentSlide, 'services__item--active');
+        } else if (e.target.closest('.services__arrow--left') && currentSlide !== 0) {
+          prevSlide(sliderItems, currentSlide, 'services__item--active');
+        }
       }
     });
   });
