1bf32c49dca9d522e0e06f80b814b0deca98600e
ferucci <development.first1@gmail.com>
2022-05-19 23:37:25 +0300

    added modal img block

--- src/modules/modal-magnification.js
+++ src/modules/modal-magnification.js
@@ -1,7 +1,47 @@
 'use strict';
 
 const imageMagnification = () => {
-  console.log('.sertificate-document')
+  const imagesCollection = document.querySelectorAll('.sertificate-document');
+
+  const overlayItem = document.querySelectorAll('.document-overlay');
+  const overlayBlock = document.querySelector('.overlay');
+
+  imagesCollection.forEach((img, index) => {
+
+    img.classList.add('document-inner');
+
+    img.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const target = e.target.closest('.sertificate-document');
+      const imageV = `<img class="modal-img" style="border-radius:20px;" src="${target}">`;
+
+      if (e.target.closest('.modal-img')) {
+        return;
+      } else if (img === target) {
+
+        overlayItem.forEach((item, id) => {
+
+          if (id === index) {
+            img.innerHTML += imageV;
+            overlayBlock.style.display = 'block';
+          }
+
+        });
+      }
+
+    });
+
+  });
+
+  overlayBlock.addEventListener('click', () => {
+    document.querySelectorAll('.modal-img').forEach(item => {
+      item.remove();
+    })
+  });
+
+
+
 }
 
 export default imageMagnification
